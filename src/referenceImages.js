9d6a3ffab897de42584372e9159fc3f9d406b1a5
S3BOLD <vinicius.sebold@alunos.sc.senac.br>
2026-08-04 21:32:47 -0300

    teste

--- src/referenceImages.js
+++ src/referenceImages.js
@@ -21,4 +21,10 @@ export const REFERENCE_IMAGES = [
     link: 'https://www.mercadolivre.com.br/kit-10-potes-hermeticos-alimentos-480-1000-2200ml-paramount/p/MLB23666833?pdp_filters=item_id%3AMLB7183851344&matt_tool=38524122#origin=share&sid=share&wid=MLB7183851344&action=copy', // cole aqui o link da página do produto
     caption: 'Kit 10 Potes Herméticos Alimentos',
   },
+  {
+    src:'https://http2.mlstatic.com/D_NQ_NP_2X_621711-MLA113105526410_072026-F-cobertor-coberdrom-edredom-casal-queen-grosso-macio-inverno.webp',
+    link:'https://www.mercadolivre.com.br/cobertor-coberdrom-edredom-casal-queen-grosso-macio-inverno/up/MLBU4274079218?pdp_filters=item_id%3AMLB4871031701&attributes=COLOR%3AMLBU4274079218&matt_tool=38524122#origin=share&sid=share&wid=MLB4871031701&action=copy',
+    caption: 'Cobertor Coberdrom Edredom Casal Queen'
+  }
 ];
+  
