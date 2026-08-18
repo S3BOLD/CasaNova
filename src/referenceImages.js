bc3e635608bef65494eb1c7b50310b801b448a83
S3BOLD <vinicius.sebold@alunos.sc.senac.br>
2026-08-17 21:06:35 -0300

    Adição de referencias

--- src/referenceImages.js
+++ src/referenceImages.js
@@ -25,6 +25,26 @@ export const REFERENCE_IMAGES = [
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_621711-MLA113105526410_072026-F-cobertor-coberdrom-edredom-casal-queen-grosso-macio-inverno.webp',
     link:'https://www.mercadolivre.com.br/cobertor-coberdrom-edredom-casal-queen-grosso-macio-inverno/up/MLBU4274079218?pdp_filters=item_id%3AMLB4871031701&attributes=COLOR%3AMLBU4274079218&matt_tool=38524122#origin=share&sid=share&wid=MLB4871031701&action=copy',
     caption: 'Cobertor Coberdrom Edredom Casal Queen'
+  },
+  {
+    src:'https://m.media-amazon.com/images/I/61SGEUH-vXL._AC_SX679_.jpg',
+    link:'https://a.co/d/0jemOAyK',
+    caption: 'Jogo de Facas'
+  },
+  {
+    src:'https://http2.mlstatic.com/D_NQ_NP_2X_902723-MLB112219066782_062026-F-kit-com-7654-3-formas-assadeiras-antiaderentes--tortab.webp',
+    link:'https://www.mercadolivre.com.br/kit-com-7654-3-formas-assadeiras-antiaderentes--tortab/up/MLBU4103933483?pdp_filters=item_id%3AMLB4786689133&attributes=CUSTOM_ESCOLHA%20SEU%20KIT%3AMLBU4103933483&matt_tool=38524122#origin=share&sid=share&wid=MLB4786689133&action=copy',
+    caption: 'Assadeiras'
+  },
+  {
+    src:'https://oxfordporcelanas.vtexassets.com/arquivos/ids/197001-1200-1200?v=638918289273870000&width=1200&height=1200&aspect=true',
+    link:'https://www.oxfordporcelanas.com.br/reagente-ryo-maresia-caneca260/p?idsku=60602983&utm_source=google&utm_medium=cpc&utm_campaign=21148750230-Pmax-Novos-Clientes&gad_source=1&gad_campaignid=21155641121&gclid=Cj0KCQjwv4XUBhDBARIsAE6bQUQcJVEuDPfrjszhoU94OdWldu7ymvdsGHopN7Dcygv1ZSSCZ8961IEaAmfwEALw_wcB',
+    caption: 'Jogo de canecas'
+  },
+  {
+    src:'https://http2.mlstatic.com/D_NQ_NP_2X_833495-MLB82421261479_022025-F-kit-4pcs-cozinha-escorredor-massa-arroz-telado-cesto-fritura.webp',
+    link:'https://www.mercadolivre.com.br/kit-4pcs-cozinha-escorredor-massa-arroz-telado-cesto-fritura/up/MLBU3008878235?pdp_filters=item_id%3AMLB3974524573&matt_tool=38524122#origin=share&sid=share&wid=MLB3974524573&action=copy',
+    caption: 'Escorredores'
   }
 ];
   
