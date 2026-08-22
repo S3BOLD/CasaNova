69a01fbc3e2c77dc5864f4e8f072741a72ae626f
Vinicius Sebold <vinicius.sebold@alunos.sc.senac.br>
2026-08-22 20:19:16 -0300

    Edição de referencias

--- src/referenceImages.js
+++ src/referenceImages.js
@@ -19,12 +19,12 @@ export const REFERENCE_IMAGES = [
   {
     src: 'https://http2.mlstatic.com/D_NQ_NP_2X_618189-MLA110999678475_042026-F.webp',
     link: 'https://www.mercadolivre.com.br/kit-10-potes-hermeticos-alimentos-480-1000-2200ml-paramount/p/MLB23666833?pdp_filters=item_id%3AMLB7183851344&matt_tool=38524122#origin=share&sid=share&wid=MLB7183851344&action=copy', // cole aqui o link da página do produto
-    caption: 'Kit 10 Potes Herméticos Alimentos',
+    caption: 'Potes Herméticos Acrilico',
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_621711-MLA113105526410_072026-F-cobertor-coberdrom-edredom-casal-queen-grosso-macio-inverno.webp',
     link:'https://www.mercadolivre.com.br/cobertor-coberdrom-edredom-casal-queen-grosso-macio-inverno/up/MLBU4274079218?pdp_filters=item_id%3AMLB4871031701&attributes=COLOR%3AMLBU4274079218&matt_tool=38524122#origin=share&sid=share&wid=MLB4871031701&action=copy',
-    caption: 'Cobertor Coberdrom Edredom Casal Queen'
+    caption: 'Coberderom'
   },
   {
     src:'https://m.media-amazon.com/images/I/61SGEUH-vXL._AC_SX679_.jpg',
@@ -39,7 +39,7 @@ export const REFERENCE_IMAGES = [
   {
     src:'https://oxfordporcelanas.vtexassets.com/arquivos/ids/197001-1200-1200?v=638918289273870000&width=1200&height=1200&aspect=true',
     link:'https://www.oxfordporcelanas.com.br/reagente-ryo-maresia-caneca260/p?idsku=60602983&utm_source=google&utm_medium=cpc&utm_campaign=21148750230-Pmax-Novos-Clientes&gad_source=1&gad_campaignid=21155641121&gclid=Cj0KCQjwv4XUBhDBARIsAE6bQUQcJVEuDPfrjszhoU94OdWldu7ymvdsGHopN7Dcygv1ZSSCZ8961IEaAmfwEALw_wcB',
-    caption: 'Jogo de canecas'
+    caption: 'Jogo de Canecas'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_833495-MLB82421261479_022025-F-kit-4pcs-cozinha-escorredor-massa-arroz-telado-cesto-fritura.webp',
@@ -59,27 +59,27 @@ export const REFERENCE_IMAGES = [
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_981634-MLA114600248639_072026-F.webp',
     link:'https://www.mercadolivre.com.br/kit-5-tigelas-bowl-redondas-em-aco-inox-para-preparo-e-servir-master-cook/p/MLB75495019?pdp_filters=item_id%3AMLB7176573630&matt_tool=38524122#origin=share&sid=share&wid=MLB7176573630&action=copy',
-    caption: 'Tigelas Bowl'
+    caption: 'Tigelas - Bowls'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_658680-MLA99600121444_122025-F.webp',
     link:'https://www.mercadolivre.com.br/kit-12-utensilios-de-silicone-com-cabo-de-madeira-marca-aristus-com-alta-resistencia-duravel-e-com-design-moderno-perfeito-para-sua-cozinha-cor-cinza/p/MLB57662272?pdp_filters=item_id%3AMLB6834079956&matt_tool=38524122#origin=share&sid=share&wid=MLB6834079956&action=copy',
-    caption: 'Kit itensilios  '
+    caption: 'Kit Utensilios  '
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_958284-MLA99496908752_112025-F.webp',
     link:'https://www.mercadolivre.com.br/batedeira-bella-massa-mondial-500w-b-23-np/p/MLB16368824?pdp_filters=item_id%3AMLB2009171518&attributes=COLOR%3AMLB16368824%2CVOLTAGE%3AMLB16368824&matt_tool=38524122#origin=share&sid=share&wid=MLB2009171518&action=copy',
-    caption: 'Batedeira Bella'
+    caption: 'Batedeira'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_969346-MLB81970534244_022025-F-abridor-de-latas-automatico-profissional-inox-sem-rebarbas.webp',
     link:'https://www.mercadolivre.com.br/abridor-de-latas-automatico-profissional-inox-sem-rebarbas/up/MLBU1110685287?pdp_filters=item_id%3AMLB2739749073&matt_tool=38524122#origin=share&sid=share&wid=MLB2739749073&action=copy',
-    caption: 'Abridor de Latas'
+    caption: 'Abridor de Latas e Garrafas'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_992424-MLB113413367918_072026-F-jogo-3-frigideiras-inducao-antiaderente-ceramica-pedra.webp',
     link:'https://www.mercadolivre.com.br/jogo-3-frigideiras-inducao-antiaderente-ceramica-pedra/up/MLBU4324624623?pdp_filters=item_id%3AMLB7185053812&attributes=COLOR%3AMLBU4324624623&matt_tool=38524122#origin=share&sid=share&wid=MLB7185053812&action=copy',
-    caption: 'Kit Frigideiras'
+    caption: 'Kit Frigideiras Antiaderentes'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_998411-MLA115257272447_072026-F.webp',
@@ -89,12 +89,12 @@ export const REFERENCE_IMAGES = [
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_814669-MLB111144812853_052026-F-kit-porta-guardanapo-e-papel-toalha-bambu-rustico-suporte.webp',
     link:'https://www.mercadolivre.com.br/kit-porta-guardanapo-e-papel-toalha-bambu-rustico-suporte/up/MLBU3931730427?pdp_filters=item_id%3AMLB6706905800&matt_tool=38524122#origin=share&sid=share&wid=MLB6706905800&action=copy',
-    caption: 'Porta guardanapos e papel toalha – madeira '
+    caption: 'Porta Guardanapos e Papel Toalha'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_908773-MLB114046910881_072026-F-jogo-6-tacas-vinho-tinto-450ml-cristal-titanium-gastro.webp',
     link:'https://www.mercadolivre.com.br/jogo-6-tacas-vinho-tinto-450ml-cristal-titanium-gastro/up/MLBU610020055?pdp_filters=item_id%3AMLB4931476362&matt_tool=38524122#origin=share&sid=share&wid=MLB4931476362&action=copy',
-    caption: 'Taças de vinho'
+    caption: 'Taças de Vinho'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_969681-MLA112308821144_062026-F.webp',
@@ -104,22 +104,48 @@ export const REFERENCE_IMAGES = [
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_868786-MLA114708738712_082026-F.webp',
     link:'https://www.mercadolivre.com.br/jogo-de-6-copos-alto-320ml-miami-vidro-grosso-moderno-luxo-transparente-quadrado-viacruz/p/MLB77196120?pdp_filters=item_id%3AMLB4964338485&matt_tool=38524122#origin=share&sid=share&wid=MLB4964338485&action=copy',
-    caption: 'Jogo de copo long '
+    caption: 'Jogo de Copos Long '
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_717014-MLB111198260186_052026-F-jogo-6-copo-de-vidro-canelado-versalhes-graffiato-420ml-luxo.webp',
     link:'https://www.mercadolivre.com.br/jogo-6-copo-de-vidro-canelado-versalhes-graffiato-420ml-luxo/up/MLBU3872904904?pdp_filters=item_id%3AMLB4563831335&matt_tool=38524122#origin=share&sid=share&wid=MLB4563831335&action=copy',
-    caption: 'Jogo de copo graffiato '
+    caption: 'Jogo de Copos Graffiato '
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_807083-MLB115214008873_072026-F-jogo-6-tacas-caneladas-de-vidro-380ml-transparente.webp',
     link:'https://www.mercadolivre.com.br/jogo-6-tacas-caneladas-de-vidro-380ml-transparente/up/MLBU4423236150?pdp_filters=item_id%3AMLB7238965536&matt_tool=38524122#origin=share&sid=share&wid=MLB7238965536&action=copy',
-    caption: 'Jogo de taças - Suco'
+    caption: 'Jogo de Taças'
   },
   {
     src:'https://http2.mlstatic.com/D_NQ_NP_2X_612882-MLB114914038163_072026-F-kit-6-tacas-vidro-canelado-sobremesa-mousse-premium-320ml.webp',
     link:'https://www.mercadolivre.com.br/kit-6-tacas-vidro-canelado-sobremesa-mousse-premium-320ml/up/MLBU4392752614?pdp_filters=item_id%3AMLB7218647456&matt_tool=38524122#origin=share&sid=share&wid=MLB7218647456&action=copy',
-    caption: 'Taças de sobremesa'
+    caption: 'Taças de Sobremesa'
   },
+  {
+    src:'https://http2.mlstatic.com/D_NQ_NP_2X_789324-MLA82919264927_032025-F-cortador-fatiador-legumes-profissional-triturador-lamina-aco.webp',
+    link:'https://www.mercadolivre.com.br/cortador-fatiador-legumes-profissional-triturador-lamina-aco/up/MLBU3056478870?pdp_filters=item_id%3AMLB3995396761&matt_tool=38524122#origin=share&sid=share&wid=MLB3995396761&action=copy',
+    caption:'Ralador'
+  },
+  {
+    src:'https://http2.mlstatic.com/D_NQ_NP_2X_656887-MLA113963475859_062026-F.webp',
+    link:'https://www.mercadolivre.com.br/kit-6-conjunto-potes-vidro-marmita-hermetico-640ml-fitiness-transparente/p/MLB74598116?pdp_filters=item_id%3AMLB7317386450&matt_tool=38524122#origin=share&sid=share&wid=MLB7317386450&action=copy',
+    caption:'Potes de Vidro'
+  },
+  {
+    src:'https://m.media-amazon.com/images/I/917d07JP0ML._AC_SL1500_.jpg',
+    link:'https://a.co/d/0gUayLY9',
+    caption:'Jogo de Xicaras'
+  },
+  {
+    src:'https://m.media-amazon.com/images/I/61mDXbMLe4L._AC_SX679_.jpg',
+    link:'https://m.media-amazon.com/images/I/61mDXbMLe4L._AC_SX679_.jpg',
+    caption:'Potes Hermeticos Plasticos '
+  },
+  {
+    src:'https://http2.mlstatic.com/D_NQ_NP_2X_833874-MLU79332853197_092024-F.webp',
+    link:'https://www.mercadolivre.com.br/kit-8-potes-hermetico-porta-mantimentos-travas-shark-blue/p/MLB40320304?pdp_filters=item_id%3AMLB3892040635&matt_tool=38524122#origin=share&sid=share&wid=MLB3892040635&action=copy',
+    caption:'Potes Hermeticos Acrilico'
+  }
+  
 ];
   
