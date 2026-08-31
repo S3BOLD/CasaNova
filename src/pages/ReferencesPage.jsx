81b1aa26c0e48e8a2a2707543e0dc8b41d0e476f
Vinicius Sebold <vinicius.sebold@alunos.sc.senac.br>
2026-08-31 00:10:16 -0300

    TesteImagem

--- src/pages/ReferencesPage.jsx
+++ src/pages/ReferencesPage.jsx
@@ -4,7 +4,7 @@ export default function ReferencesPage() {
   return (
     <main className="page references-page">
       <div className="page-header">
-        <img src="src\referencias\Amandoca.jpeg"  className="page-header-img" />
+        <img src="src/referencias/Amandoca.jpeg"  className="page-header-img" />
         <h2 className="page-title">Aqui estão algumas referências  dos itens listados, com isso você pode ter mais referências do que escolher :)</h2>
         
       </div>
